36d5360c1d8452bdca8e1cde69f9a0c7d8254137
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-10 02:49:06 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,9 +19,9 @@ contacts.each do |person, data|
     if attribute == :favorite_icecream_flavors
      value.delete_if {|flavor| flavor == "strawberry"}
    end
- end 
+  end 
  end
-
+ contacts
   #remember to return your newly altered contacts hash!
   contacts
 end
